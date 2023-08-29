b5ed22c1b848064fcb36a9140db597886a9e7a6f
Dali <chaouch.mohammed.ali@gmail.com>
2023-08-29 21:53:32 +0100

    server third commit

--- models/orderSchema.js
+++ models/orderSchema.js
@@ -1,17 +1,13 @@
 const mongoose=require('mongoose')
 const orderSchema=mongoose.Schema({
-    item:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'menu'},
+    item:{type:mongoose.Schema.Types.ObjectId,ref:'menu'},
     numberOfItems:{type:Number,required:true},
     createAt:{
         type:Date,
         default:new Date() },
        owner:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:'user'
-       }
-    
+        ref:'user'}
     
 })
 
