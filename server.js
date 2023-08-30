357c9b9ae148cbc3c62ac5a1287127d782e35438
Dali <chaouch.mohammed.ali@gmail.com>
2023-08-30 20:36:12 +0100

    last server commit

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const dotenv=require('dotenv')
 dotenv.config({path:__dirname+"/.env"})
 const connectDB=require('./config/connectDB')
 const port=process.env.PORT||8000
+const path = require('path');
 app.use(express.json())
 app.use(cors())
 connectDB()
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/api',require('./routes/adminRoutes'))
 app.use('/api',require('./routes/userRoutes'))
 app.use('/api',require('./routes/menuRoutes'))
